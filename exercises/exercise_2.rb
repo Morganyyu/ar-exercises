bbc6f58502d4f9e4bb3cee2fadcbcda3ac8d7c9f
Morganyyu <morganyu120@hotmail.com>
2018-04-24 21:19:49 +0000

    completed up to exercise 6

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Store.find(1).instance_variable_set(:@store1).update(name "Langley")
+# Store.find(2).instance_variable_set(:@store2)
+@store1 = Store.find(1).update(name: "Vancouver")
+@store2 = Store.find(2)
+
+
+puts @store1
+puts @store2
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  # has_many :employees
+  # validates :name,
+  #           presence: true
+  #           length: { minimum: 3 }
+  # validates :annual_revenue,
+  #           numericality: true
+  #           minimum: 0
+  # attr_accessor :store1, :store2
+  # def initialize
 
+  # end
 end
